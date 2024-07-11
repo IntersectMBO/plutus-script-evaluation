d9a06c04130b23738857bc02ab524aaee6e58aac
Yura Lazarev <Unisay@users.noreply.github.com>
2024-07-11 14:30:27 +0200

    Initial sources

---
+++ README.md
@@ -0,0 +1,17 @@
+# Plutus Script Evaluation Test
+
+## How to use
+
+0. Create `.envrc.local` with the following content (adjust the paths as needed):
+   ```sh
+    export CARDANO_NODE_SOCKET_PATH="/home/yura/projects/cardano/node/node-state/sanchonet/node.sock"
+    export CARDANO_NODE_CONFIG_PATH="/home/yura/projects/cardano/cardano-playground/docs/environments/sanchonet/config.json"
+   ```
+1. Enter the `nix` shell using either `nix develop` command or `direnv` hooked to your shell.
+2. See available commands by entering `info` in the shell.
+3. Run the script dump job using the `dump` command.
+
+### Checkpoints
+
+Only the latest two checkpoint files are kept due to their large sizes.
+We keep two checkpoint files rather than one in case the latest one is corrupt (e.g., the job is killed while the file is being uploaded), or the latest one is invalid due to a rollback.
---
+++ nix/shell.nix
@@ -0,0 +1,52 @@
+# Docs for this file: https://github.com/input-output-hk/iogx/blob/main/doc/api.md#mkhaskellprojectinshellargs
+# See `shellArgs` in `mkHaskellProject` in ./project.nix for more details.
+
+{ repoRoot
+, inputs
+, pkgs
+, lib
+, system
+,
+}:
+
+# Each flake variant defined in your project.nix project will yield a separate
+# shell. If no flake variants are defined, then cabalProject is the original project.
+cabalProject: {
+  name = "plutus-script-evaluation";
+
+  packages = [ pkgs.figlet ];
+
+  scripts = {
+    dump = {
+      description = "Dump Plutus Script events from mainnet";
+      group = "general";
+      exec = ''
+        cabal run dump-script-events -- \
+          --mainnet \
+          --socket-path "$CARDANO_NODE_SOCKET_PATH" \
+          --config "$CARDANO_NODE_CONFIG_PATH" \
+          --blocks-per-file 50000 \
+          --events-per-file 50000 \
+          --dump-dir dumps \
+          --checkpoint-dir dumps/checkpoints \
+      '';
+    };
+  };
+
+  # env = {
+  #   KEY = "VALUE";
+  # };
+
+  shellHook = ''
+    figlet "Plutus Script Evaluation Test"
+  '';
+
+  preCommit = {
+    cabal-fmt.enable = true;
+    stylish-haskell.enable = false;
+    fourmolu.enable = true;
+    hlint.enable = true;
+    editorconfig-checker.enable = true;
+    nixpkgs-fmt.enable = true;
+  };
+}
