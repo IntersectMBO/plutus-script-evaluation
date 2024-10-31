b41387960e599a628fa4796581a026896663b320
Yuriy Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-10-31 12:15:01 +0100

    Deserialise scripts and materialise view after loading events.

--- plutus-script-evaluation/materialise-views/Main.hs
+++ plutus-script-evaluation/materialise-views/Main.hs
@@ -1,9 +1,9 @@
-module Main (main) where
+module Main where
 
 import Control.Exception (bracket)
 import Database.PostgreSQL.Simple (close, connectPostgreSQL)
-import Deserialise (deserialiseScripts)
 import Main.Utf8 (withUtf8)
+import Materialise (materialiseViews)
 import Options (Options (..), parserInfo)
 import Options.Applicative (execParser)
 import System.IO (BufferMode (LineBuffering), hSetBuffering, stdin, stdout)
@@ -13,4 +13,4 @@ main = withUtf8 do
   hSetBuffering stdin LineBuffering
   hSetBuffering stdout LineBuffering
   Options{optsDatabaseConnStr} <- execParser parserInfo
-  bracket (connectPostgreSQL optsDatabaseConnStr) close deserialiseScripts
+  bracket (connectPostgreSQL optsDatabaseConnStr) close materialiseViews
