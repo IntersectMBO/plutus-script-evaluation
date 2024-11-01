feeb0545c1775ad37c19099dddba54d8d09bddc8
Yuriy Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-11-01 12:02:03 +0100

    Gracefully stop chain sync.

--- plutus-script-evaluation/lib/Streaming.hs
+++ plutus-script-evaluation/lib/Streaming.hs
@@ -7,29 +7,21 @@ module Streaming (
 
 import Cardano.Api (SocketPath)
 import Cardano.Api.ChainSync.Client (
-  ClientStIdle (SendMsgFindIntersect, SendMsgRequestNext),
+  ClientStIdle (SendMsgDone, SendMsgFindIntersect, SendMsgRequestNext),
   ClientStIntersect (
     ClientStIntersect,
     recvMsgIntersectFound,
     recvMsgIntersectNotFound
   ),
-  ClientStNext (
-    ClientStNext,
-    recvMsgRollBackward,
-    recvMsgRollForward
-  ),
+  ClientStNext (ClientStNext, recvMsgRollBackward, recvMsgRollForward),
  )
 import Cardano.Api.Shelley qualified as C
 import Cardano.Slotting.Block (BlockNo (..))
 import Cardano.Slotting.Slot (WithOrigin (At, Origin), withOrigin)
-import Control.Concurrent.Async (ExceptionInLinkedThread (..), link, withAsync)
-import Control.Concurrent.MVar (newEmptyMVar, putMVar, takeMVar)
-import Control.Exception (Exception, SomeException (..), handle, throw)
-import Control.Monad (forever, when)
+import Control.Exception (Exception, throw)
 import Control.Monad.IO.Class (liftIO)
 import GHC.Generics (Generic)
 import GHC.Word (Word64)
-import System.Exit (exitSuccess)
 
 {- | `subscribeToChainSyncEvents` uses the chain-sync mini-protocol to
 connect to a locally running node and fetch blocks from the given
@@ -44,81 +36,69 @@ subscribeToChainSyncEvents
   -> (ChainSyncEvent -> IO ())
   -- ^ Callback receiving the blocks
   -> IO ()
-subscribeToChainSyncEvents socketPath networkId points callback = do
-  nextChainSyncEvent <- newEmptyMVar
-  let
-    connection =
-      C.connectToLocalNode
-        C.LocalNodeConnectInfo
-          { C.localConsensusModeParams =
-              -- This a parameter needed only for the Byron era.
-              -- Since the Byron era is over and the parameter has never
-              -- changed it is ok to hardcode this.
-              C.CardanoModeParams (C.EpochSlots 21600)
-          , C.localNodeNetworkId = networkId
-          , C.localNodeSocketPath = socketPath
-          }
-        C.LocalNodeClientProtocols
-          { C.localChainSyncClient =
-              C.LocalChainSyncClient . C.ChainSyncClient . pure $
-                SendMsgFindIntersect points onIntersect
-          , C.localStateQueryClient = Nothing
-          , C.localTxMonitoringClient = Nothing
-          , C.localTxSubmissionClient = Nothing
-          }
+subscribeToChainSyncEvents socketPath networkId points callback =
+  C.connectToLocalNode
+    C.LocalNodeConnectInfo
+      { C.localConsensusModeParams =
+          -- This a parameter needed only for the Byron era.
+          -- Since the Byron era is over and the parameter has never
+          -- changed it is ok to hardcode this.
+          C.CardanoModeParams (C.EpochSlots 21600)
+      , C.localNodeNetworkId = networkId
+      , C.localNodeSocketPath = socketPath
+      }
+    C.LocalNodeClientProtocols
+      { C.localChainSyncClient =
+          C.LocalChainSyncClient . C.ChainSyncClient . pure $
+            SendMsgFindIntersect points onIntersect
+      , C.localStateQueryClient = Nothing
+      , C.localTxMonitoringClient = Nothing
+      , C.localTxSubmissionClient = Nothing
+      }
+ where
+  onIntersect =
+    ClientStIntersect
+      { recvMsgIntersectFound = \chainPoint tip ->
+          C.ChainSyncClient do
+            callback (RollBackward chainPoint tip)
+            sendRequestNext
+      , recvMsgIntersectNotFound = throw NoIntersectionFound
+      }
 
-    onIntersect =
-      ClientStIntersect
-        { recvMsgIntersectFound = \chainPoint tip ->
+  sendRequestNext =
+    pure $ SendMsgRequestNext (pure ()) do
+      ClientStNext
+        { recvMsgRollForward = \blockInMode tip ->
             C.ChainSyncClient do
-              putMVar nextChainSyncEvent (RollBackward chainPoint tip)
+              let blockNo = bimBlockNo blockInMode
+              let volatileTip = fromChainTip tip
+              let tipBlockNo = withOrigin 0 unBlockNo volatileTip
+              let immutableTip = BlockNo (tipBlockNo - securityParam)
+
+              putStrLn $
+                "Roll forward to "
+                  ++ show (bimSlotNo blockInMode)
+                  ++ ", Current "
+                  ++ show blockNo
+                  ++ ", Tip "
+                  ++ show volatileTip
+              callback (RollForward blockInMode tip)
+
+              if blockNo < immutableTip
+                then sendRequestNext
+                else liftIO do
+                  SendMsgDone ()
+                    <$ putStrLn
+                      ( "Reached immutable tip ("
+                          ++ show immutableTip
+                          ++ "), exiting. "
+                      )
+        , recvMsgRollBackward = \chainPoint tip ->
+            C.ChainSyncClient do
+              callback (RollBackward chainPoint tip)
               sendRequestNext
-        , recvMsgIntersectNotFound = throw NoIntersectionFound
         }
 
-    sendRequestNext =
-      pure $ SendMsgRequestNext (pure ()) do
-        ClientStNext
-          { recvMsgRollForward = \blockInMode tip ->
-              C.ChainSyncClient do
-                let blockNo = bimBlockNo blockInMode
-                let volatileTip = fromChainTip tip
-                let tipBlockNo = withOrigin 0 unBlockNo volatileTip
-                let immutableTip = BlockNo (tipBlockNo - securityParam)
-
-                putStrLn $
-                  "Roll forward to "
-                    ++ show (bimSlotNo blockInMode)
-                    ++ ", Current "
-                    ++ show blockNo
-                    ++ ", Tip "
-                    ++ show volatileTip
-                putMVar nextChainSyncEvent (RollForward blockInMode tip)
-
-                when (blockNo > immutableTip) $ liftIO do
-                  putStrLn $
-                    "Reached immutable tip ("
-                      ++ show immutableTip
-                      ++ "), exiting. "
-                  exitSuccess
-
-                sendRequestNext
-          , recvMsgRollBackward = \chainPoint tip ->
-              C.ChainSyncClient do
-                putMVar nextChainSyncEvent (RollBackward chainPoint tip)
-                sendRequestNext
-          }
-
-  -- Let's rethrow exceptions from the client thread unwrapped, so that the
-  -- callback does not have to know anything about async
-  handle (\(ExceptionInLinkedThread _async (SomeException e)) -> throw e) $
-    withAsync connection \a -> do
-      -- Make sure all exceptions in the client thread
-      -- are passed to the callback thread
-      link a
-      -- Run the callback forever
-      forever (takeMVar nextChainSyncEvent >>= callback)
- where
   bimBlockNo :: C.BlockInMode -> C.BlockNo
   bimBlockNo (C.BlockInMode _era (C.Block (C.BlockHeader _ _ blockNo) _)) =
     blockNo
