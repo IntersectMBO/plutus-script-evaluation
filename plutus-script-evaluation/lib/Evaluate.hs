61efa4b550f57561c329647c31ecf95b936b6bba
Yuriy Lazaryev <Yuriy.Lazaryev@iohk.io>
2024-12-27 12:48:09 +0100

    Evaluation job operates in the MonadUnliftIO m instead of IO

--- plutus-script-evaluation/lib/Evaluate.hs
+++ plutus-script-evaluation/lib/Evaluate.hs
@@ -3,6 +3,7 @@
 module Evaluate where
 
 import Codec.Serialise (deserialise)
+import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Trans.Except (runExceptT)
 import Control.Monad.Trans.Writer (WriterT (runWriterT))
 import Data.ByteString qualified as BSL
@@ -35,20 +36,23 @@ data ScriptEvaluationInput = MkScriptEvaluationInput
   }
 
 evaluateScripts
-  :: Postgres.Connection
+  :: (MonadFail m, MonadUnliftIO m)
+  => Postgres.Connection
   -- ^ Database connection
   -> a
   -- ^ Initial accumulator
-  -> (ScriptEvaluationInput -> a -> IO a)
+  -> (ScriptEvaluationInput -> a -> m a)
   -- ^ Accumulation function
-  -> IO a
+  -> m a
 evaluateScripts conn initialAccum accumulate =
   Db.withScriptEvaluationEvents conn initialAccum \accum record -> do
     scriptInput <- inputFromRecord record
     accumulate scriptInput accum
 
 inputFromRecord
-  :: (MonadFail m) => Db.ScriptEvaluationRecord -> m ScriptEvaluationInput
+  :: (MonadFail m)
+  => Db.ScriptEvaluationRecord
+  -> m ScriptEvaluationInput
 inputFromRecord MkScriptEvaluationRecord'{..} = do
   let mkEvalCtx f =
         runExceptT (runWriterT f) >>= \case
